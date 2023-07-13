4661b13a237ce8ede7ce7e925d66b3bde641f2c2
huangnan <2544563843@qq.com>
2023-07-13 18:53:07 +0800

    2023-7-13 parse .m3u8 file

--- server/src/main/java/io/livevideo/server/controller/VideoSourceController.java
+++ server/src/main/java/io/livevideo/server/controller/VideoSourceController.java
@@ -1,27 +1,31 @@
 package io.livevideo.server.controller;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.time.Duration;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
-import java.util.Set;
 import java.util.concurrent.TimeUnit;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import org.openqa.selenium.By;
+import org.checkerframework.checker.units.qual.A;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import io.livevideo.server.utils.ResultCode;
 import io.livevideo.server.utils.myResult;
+import io.livevideo.server.utils.myHttpClient;
 
 
 @RestController
@@ -29,50 +33,69 @@ public class VideoSourceController{
 
     @Autowired
     private ChromeOptions WebDriverOptions;
+    @Autowired
+    private myHttpClient httpClient;
 
-    @RequestMapping("/getM3u8FromIframeSrcByHtml")
-    public myResult getM3u8FromIframeSrcByHtml(@RequestParam("URL") Optional<String> URL){
+    @RequestMapping("/getM3u8FromIframeByHtml")
+    public myResult getM3u8FromIframeByHtml(@RequestParam("url") Optional<String> url){
+        String link = getSrcFormIframeByEndswith(url,".m3u8");
+        return new myResult(ResultCode.SUCCESS.getCode(), ResultCode.SUCCESS.getMsg(),link);
+    }
+
+    @RequestMapping("/getMp4FromIframeByHtml")
+    public myResult getMp4FromIframeByHtml(@RequestParam("url") Optional<String> url){
+        String link = getSrcFormIframeByEndswith(url,".mp4");
+        return new myResult(ResultCode.SUCCESS.getCode(), ResultCode.SUCCESS.getMsg(),link);
+    }
+
+    @RequestMapping("/getM3u8")
+    public myResult getM3u8(@RequestParam("url") Optional<String> url){
         WebDriver driver = new ChromeDriver(WebDriverOptions);
-        if (isUrlNull(URL)){
+        if (isUrlNull(url)){
             return new myResult(ResultCode.FAILURE.getCode(),"无效URL");
         }
 
-        driver.get(URL.get());
-        driver.manage().timeouts().implicitlyWait(Duration.ofMillis(5000));
-        String link = getUrlFromHtmlByType(driver, "iframe");
-        driver.get(link);
-        // WebDriverWait wait = new WebDriverWait(driver, Duration.ofMillis(5000));
-        // wait.until(ExpectedConditions.attributeToBeNotEmpty(driver.findElement(By.tagName("video")), "src"));
-        try {
+        String link = checkM3u8Nest(url.get());
+        
+        try { 
             TimeUnit.SECONDS.sleep(1);
         } catch (InterruptedException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
         }
-        link = getUrlFromHtmlByEndswith(driver, ".m3u8");
-        driver.quit();
-        return new myResult(ResultCode.SUCCESS.getCode(), ResultCode.SUCCESS.getMsg(),link);
-    }
 
-    @RequestMapping("/getM3u8FromHtml")
-    public myResult getM3u8FromHtml(@RequestParam("URL") Optional<String> URL){
-        WebDriver driver = new ChromeDriver(WebDriverOptions);
-        if (isUrlNull(URL)){
-            return new myResult(ResultCode.FAILURE.getCode(),"无效URL");
+        driver.quit();
+        if (isStrNull(link)){
+            return new myResult(ResultCode.SUCCESS.getCode(),"没有.m3u8嵌套",true);
         }
-        driver.get(URL.get());
-        driver.manage().timeouts().implicitlyWait(Duration.ofMillis(5000));
-        String link = getUrlFromHtmlByEndswith(driver, ".m3u8");
-        driver.quit();
+        URL fullUrl;
+        try {
+            fullUrl = new URL(url.get());
+            String baseUrl = fullUrl.getProtocol() + "://" + fullUrl.getHost();
+            link = baseUrl + link;
+        } catch (MalformedURLException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    
         return new myResult(ResultCode.SUCCESS.getCode(), ResultCode.SUCCESS.getMsg(),link);
     }
 
-    public Boolean isUrlNull(Optional<String> URL){
-        if (!URL.isPresent() || URL.isEmpty()){
+    public Boolean isUrlNull(Optional<String> url){
+        if (!url.isPresent() || url.isEmpty()){
             return true;
         }
         return false;
     }
+
+    public Boolean isStrNull(String Str){
+
+        if (Str == null || Str.isEmpty()) {
+            return true;
+        }
+        return false;
+    }
+
     
     public String getUrlFromHtmlByType(WebDriver driver, String initiatorType){
         JavascriptExecutor jse = (JavascriptExecutor) driver;
@@ -93,17 +116,18 @@ public class VideoSourceController{
 
             }catch(Exception e){
                 e.printStackTrace();
+                driver.quit();
             }
         }
         return link;
     }
 
-    public String getUrlFromHtmlByEndswith(WebDriver driver, String endsWith){
+    public String getUrlFromHtmlByEndswith(WebDriver driver, String endswith){
         JavascriptExecutor jse = (JavascriptExecutor) driver;
         List<Map> xhr = (ArrayList)jse.executeScript("return window.performance.getEntries();");
 
         String link = "";
-        Pattern pattern = Pattern.compile("\\"+endsWith+"$");
+        Pattern pattern = Pattern.compile("\\"+endswith+"$");
 
         for (Map item : xhr) {
             try{
@@ -116,12 +140,58 @@ public class VideoSourceController{
                     link = (String)item.get("name");
                     return link;
                 }
-
             }catch(Exception e){
                 e.printStackTrace();
+                driver.quit();
             }
         }
-
         return link;
     }
+
+    public String getSrcFormIframeByEndswith(Optional<String> URL, String endswith){
+        WebDriver driver = new ChromeDriver(WebDriverOptions);
+        if (isUrlNull(URL)){
+            return "";
+        }
+
+        driver.get(URL.get());
+        driver.manage().timeouts().implicitlyWait(Duration.ofMillis(5000));
+        String link = getUrlFromHtmlByType(driver, "iframe");
+        driver.get(link);
+        try {
+            TimeUnit.SECONDS.sleep(1);
+        } catch (InterruptedException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+            driver.quit();
+        }
+        link = getUrlFromHtmlByEndswith(driver, endswith);
+        driver.quit();
+        return link;
+    }
+
+    public String checkM3u8Nest(String src){
+
+        String filePath = httpClient.fileDownLoad(src);
+        if (filePath == null||filePath.isEmpty()){
+            return "";
+        }
+        Pattern pattern = Pattern.compile("\\.m3u8$");
+        
+        try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                Matcher matcher = pattern.matcher(line);
+                if (matcher.find())
+                {
+                    return line;
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return "";
+    }
+
 }
---
+++ server/src/main/java/io/livevideo/server/utils/myHttpClient.java
@@ -0,0 +1,46 @@
+package io.livevideo.server.utils;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.nio.file.Path;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class myHttpClient {
+
+    @Value("${var.filePath}")
+    private String filePath;
+
+    public String fileDownLoad(String URL) {
+
+        String targetFilePath =filePath + URL.substring(URL.lastIndexOf("/"));;
+
+        HttpClient httpClient = HttpClient.newHttpClient();
+
+        HttpRequest httpRequest = HttpRequest.newBuilder()
+                .uri(URI.create(URL))
+                .GET()
+                .build();
+
+        try {
+            HttpResponse<Path> httpResponse = httpClient.send(httpRequest, HttpResponse.BodyHandlers.ofFile(Path.of(targetFilePath)));
+
+            if (httpResponse.statusCode() == 200) {
+                Path downloadedFile = httpResponse.body();
+                System.out.println("文件下载完成: " + downloadedFile);
+                return targetFilePath;
+            } else {
+                System.out.println("文件下载失败");
+                return "";
+            }
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+            return "";
+        }
+    }
+}
